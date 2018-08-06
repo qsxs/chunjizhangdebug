ce9110482c04675008b4c776d6c9c885ffbdf310
jweilan <1123217106@qq.com>
2018-08-06 12:01:43 +0800

    pc下载链接变量化时未改

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config={
-    downloadurl:"./index/chunjizhang1.apk",
+    downloadurl:"./index/chunjizhang.apk",
     downloadImg:["./index/c_1.png","./index/c_2.png","./index/c_3.png"],
     payCode:["./index/donate_wechat.jpg","./index/donate_alipay.png"]
 }
