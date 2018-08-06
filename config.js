ac41491befb7fefb693770a9c4847d9cf5d8e0c2
jweilan <1123217106@qq.com>
2018-08-06 11:00:49 +0800

    变量化了下载链接和另外一些图。。

---
+++ config.js
@@ -0,0 +1,8 @@
+var config={
+    downloadurl:"./index/chunjizhang1.apk",
+    downloadImg:["./index/c_1.png","./index/c_2.png","./index/c_3.png"],
+    payCode:["./index/donate_wechat.jpg","./index/donate_alipay.png"]
+}
+
+
+
